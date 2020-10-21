3e1d4bd474a1b8a9a341a7a0f82575ede6818932
Louisa May <mgerwick@gmail.com>
2020-10-20 19:34:58 -0500

    added psuedocode to javascript and html

---
+++ script.js
@@ -0,0 +1,8 @@
+//Button Click handler
+    //ajax call to apis
+
+//Render map
+//Render search results
+//Add clickable results to webpage or copy address (if everything else is done)
+
+//Dark and Light mode in local storage
