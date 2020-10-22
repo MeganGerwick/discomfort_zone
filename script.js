b5fe2b8e2089e36213e5a3ff55427944d0c8080a
Greg Stead <gregstead@gmail.com>
2020-10-22 16:50:29 -0500

    Fixed js styling

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //Initialize floating action button for light and dark mode
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left'
+        direction: 'left'
     });
-  });
+});
+
+
 //Button Click handler
-    //ajax call to apis
+//ajax call to apis
 
 // function getAjax(params) {
 //     console.log("I work");
@@ -18,12 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
 var discomfortMap;
 function initMap() {
     var mapOpts = {
-        center: {lat: 39.0997, lng: -94.5786},
+        center: { lat: 39.0997, lng: -94.5786 },
         zoom: 8,
     };
     discomfortMap = new google.maps.Map(document.getElementById('discomfortMap'), mapOpts);
     var location0 = new google.maps.Marker({
-        position: {lat: 39.0997, lng: -94.5786},
+        position: { lat: 39.0997, lng: -94.5786 },
         map: discomfortMap,
         title: 'Kansas City: Crown Town',
         animation: google.maps.Animation.DROP
